258798be2401a9e29e587ed4bff9dc482a2dc4dd
Herman Westerman <herman.westerman@gmail.com>
2020-07-14 11:25:25 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/genie.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/genie.dir/main.cpp.o"
+  "genie"
+  "genie.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/genie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/genie.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for genie.
+# This may be replaced when dependencies are built.
